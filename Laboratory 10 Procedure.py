4035b0b18225089bac8dd3c5bbd2aac3e1d87045
Kenneth Asugas <kennethasugas25@gmail.com>
2025-04-12 04:23:16 +0800

    Add files via upload

---
+++ Laboratory 10 Procedure.py
@@ -0,0 +1,47 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+
+# Creating tkinter window and set dimensions
+window = tk.Tk()
+window.title('Combobox')
+window.geometry('500x250')
+
+def choice(event):
+    selected_month = month.get()
+    messagebox.showinfo(title="Selection", message=f'You selected {selected_month}')
+
+# Label text for title
+ttk.Label(window, text="Choose your birth month",
+          background='light yellow', foreground="black",
+          font=("Times New Roman", 15)).grid(row=0, column=1)
+
+# Set Label
+ttk.Label(window, text="Select the month of your birth:",
+          font=("Times New Roman", 12)).grid(column=0, row=5, padx=5, pady=25)
+
+# Create Combobox
+n = tk.StringVar()
+month = ttk.Combobox(window, width=27, textvariable=n)
+
+# Adding combobox drop down list
+month['values'] = (
+                   'January',
+                   'February',
+                   'March',
+                   'April',
+                   'May',
+                   'June',
+                   'July',
+                   'August',
+                   'September',
+                   'October',
+                   'November',
+                   'December'
+)
+
+# Bind event for selection
+month.grid(column=1, row=5)
+month.bind("<<ComboboxSelected>>", choice)
+
+window.mainloop()
